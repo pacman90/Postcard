bf534dc2398936a51ce6eb7f26e9061534109016
pacman90 <Pac@Pablos-MacBook-Pro.local>
2015-02-09 16:43:18 -0500

    Commiting a challenge as we speak

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -34,6 +35,13 @@ class ViewController: UIViewController {
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        nameLabel.text = enterNameTextField.text
+        nameLabel.hidden = false
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
